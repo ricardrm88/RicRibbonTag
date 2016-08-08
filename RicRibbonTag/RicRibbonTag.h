8a51c0c484d41ad355449d476c1c8c1159a9497e
Ricard Romeo Murgo <ricard.romeo@immfly.com>
2016-08-08 19:38:05 +0200

    First commit

---
+++ RicRibbonTag/RicRibbonTag.h
@@ -0,0 +1,19 @@
+//
+//  RicRibbonTag.h
+//  RicRibbonTag
+//
+//  Created by Ricard Romeo Murgo on 8/8/16.
+//  Copyright © 2016 Ricard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RicRibbonTag.
+FOUNDATION_EXPORT double RicRibbonTagVersionNumber;
+
+//! Project version string for RicRibbonTag.
+FOUNDATION_EXPORT const unsigned char RicRibbonTagVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RicRibbonTag/PublicHeader.h>
+
+
